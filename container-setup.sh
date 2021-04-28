f90c8a6d8b9a313b8b6ce9cebea44e78990b10e0
Jason Lian <jasonlian@catalyst-au.net>
2021-04-28 17:00:30 +1000

    Add bash script to run phpunit from container

--- container-setup.sh
+++ container-setup.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
-cd /siteroot
+# Composer install
+cd /siteroot && composer install
 
-composer install
-
+# This seems to be navitas only blocks/navint, if this exists, need to run composer install inside it.
 if [[ -d "/siteroot/blocks/navint/" ]]; then
     cd /siteroot/blocks/navint/ && composer install && cd /siteroot/
 fi
 
-php admin/tool/phpunit/cli/init.php
+# Init phpunit.
+/usr/bin/php admin/tool/phpunit/cli/init.php
 
-php admin/cli/install_database.php --adminpass=soMePass123_ --agree-license --adminemail=admin@dummy.org --fullname=AdminPerson --shortname=admin
+# Run commandline DB Install.
+/usr/bin/php admin/cli/install_database.php --adminpass=soMePass123_ --agree-license --adminemail=admin@dummy.org --fullname=AdminPerson --shortname=admin
 
+# Export variable for XDEBUG.
 export XDEBUG_CONFIG="remote_host=172.17.0.1 remote_port=9000"
-
 export PHP_IDE_CONFIG="serverName=http://localhost"
---
+++ docker-phpunit.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# Get the container ID from current directory's docker-compose instance.
+webcont="$(docker-compose ps -q moodle)"
+
+# If container ID does not exist, exit.
+if [[ -z $webcont ]]; then
+    echo "Docker compose container not found... "
+    echo "Run this command when inside the directory that has docker-compose.xml file... "
+    exit
+fi
+
+# Run php unit with extra paramters, or otherwise run the complete unit test.
+if [[ ! -z $1 ]]; then
+    echo "Running phpunit with additional paramters..."
+    echo "docker exec -i $webcont /siteroot/vendor/bin/phpunit ${1} ${2}> $(pwd)/phpunit_${1}_`date +%d-%m-%Y_%H_%M_%S`.log"
+    docker exec -i $webcont bash -c "vendor/bin/phpunit \"${1}\" ${2}" > $(pwd)/phpunit_${1}_`date +%d-%m-%Y_%H_%M_%S`.log
+    exit
+else
+    echo "Command Args not found, running full phpunit test ..."
+    # docker exec -i resrel bash -c \"cd /siteroot && vendor/bin/phpunit\" > ~/Documents/monash/resrel/phpunit.log
+    echo "docker exec -i $webcont vendor/bin/phpunit > $(pwd)/phpunit_`date +%d-%m-%Y_%H_%M_%S`.log"
+    docker exec -i $webcont vendor/bin/phpunit > $(pwd)/phpunit_`date +%d-%m-%Y_%H_%M_%S`.log
+    exit
+fi
